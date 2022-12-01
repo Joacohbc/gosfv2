756b412256dc50e9635bcec398c51ab64c2db8bb
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2022-11-30 22:21:40 -0300

    Se agrega la opción para registrarse

---
+++ static/register/register.js
@@ -0,0 +1,27 @@
+import { getToken } from '/static/modules/request.js';
+import { showError}  from '/static/modules/message.js';
+
+if(getToken() != null) {
+    // Si esta logueado, lo redirijo a la pagina principal
+    window.location.href = '/static/main/files.html';
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+    // Agrego el evento click al botón de Login
+    this.document.querySelector('#btn-register').addEventListener('click', function(e) {
+        e.preventDefault();
+
+        let url = window.location.origin + '/register';
+
+        axios.post(url,{
+                username: document.querySelector("#username").value,
+                password: document.querySelector("#password").value
+        })
+        .then(req => {
+            window.location.href = '/static/login.html';
+        })
+        .catch(err => {
+            showError(err.response.data.message);
+        });
+    });
+});
