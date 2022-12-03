b5d0955630744ceff0421938220ebe608ff9ee4d
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2022-12-03 14:55:40 -0300

    Mejorando el modulo de Mensajes
    - Ahora hay 2 variables globales que permiten controlar el ID del elemento con el mensaje y otra para definir los colores del texto de los mensajes
    - El mensaje ahora no se va a los 5s si este fue cambiado (si se vuelve a llamar al método showMessage)

--- static/modules/message.js
+++ static/modules/message.js
@@ -1,33 +1,59 @@
-const colors = {
+// Objeto que contiene los colores de los mensajes
+// Que se pueden cambiar de forma global
+let colors = {
     info: 'blue',
     error: 'red',
     success: 'green',
     warning: 'yellow'
 };
 
-export function showError(message, id = "#message") {
+// Función que permite cambiar los colores de los mensajes
+// Que se pueden cambiar de forma global
+// Debe contendrá un objeto con los colores de los mensajes
+// - info
+// - error
+// - success
+// - warning
+export function setColors(newColors) {
+    colors = newColors;
+}
+
+// Variable que determina el ID (Global) del elemento que mostrará el mensaje
+let messageId = "message";
+
+// Función que permite cambiar el ID (Global) del elemento que mostrará el mensaje
+// Debe contener el ID del elemento que mostrará el mensaje
+export function setMessageId(id) {
+    messageId = id;
+}
+
+export function showError(message, id = messageId) {
     showMessage(message, colors.error, id);
 }
 
-export function showSuccess(message, id = "#message") {
+export function showSuccess(message, id = messageId) {
     showMessage(message, colors.success, id);
 }
 
-export function showWarning(message, id = "#message") {
+export function showWarning(message, id = messageId) {
     showMessage(message, colors.warning, id);
 }
 
-export function showInfo(message, id = "#message") {
+export function showInfo(message, id = messageId) {
     showMessage(message, colors.info, id);
 }
 
-
 function showMessage(message, color, id) {
-    const element = document.querySelector(id);
+    const element = document.getElementById(id);
     element.style.color = color;
     element.style.fontWeight = "bold";
     element.innerHTML = message[0].toUpperCase() + message.substring(1);
     setTimeout(() => {
-        element.innerHTML = '';
+        // Si el mensaje que se muestra es el mismo que se quiere ocultar
+        // Ya que si hay otro mensaje (porque se llamo en otra instancia al método
+        // que este no borre el mensaje que se quiere mostrar)
+        if(message == element.innerHTML){
+            element.innerHTML = '';
+        }
     }, 5000);
 }
