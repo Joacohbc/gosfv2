b6d04cfd217215044cc07fe1578ad6715a90bf58
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2024-07-04 23:29:00 -0300

    Update build script to include cross-compilation for Linux and Windows

--- build.sh
+++ build.sh
@@ -3,6 +3,12 @@ npm run build
 cp ./sw.js ../static
 
 cd ..
+
+cd ./src/
+env GOOS=linux GOARCH=amd64 go build -o ../bin/app-gosf.bin
+env GOOOS=windows GOARCH=amd64 go build -o ../bin/app-gosf.exe
+
+cd ..
 docker compose down -v;
 docker compose build --no-cache;
 docker compose --env-file ./config.env up -d;
