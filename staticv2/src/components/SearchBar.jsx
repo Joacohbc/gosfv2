1bc2cb9677baf6c6d4729206b653786a043ad4cf
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2024-07-14 23:02:20 -0300

    Add shared filter on Search bar

--- staticv2/src/components/SearchBar.jsx
+++ staticv2/src/components/SearchBar.jsx
@@ -1,8 +1,9 @@
 import './SearchBar.css';
-import { useCallback, useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import { handleKeyUpWithTimeout } from '../utils/input-text';
 import PropTypes from "prop-types";
 import ToolTip from './ToolTip';
+import { useWhatChanged } from '@simbathesailor/use-what-changed';
 
 const sortByName = (order) => {
     return (data) => data.sort((a, b) => {
@@ -34,6 +35,16 @@ const sortByName = (order) => {
 
 const filterByText = (text) => {
     return (data) => data.filter(file => file.filename.toLowerCase().includes(text.toLowerCase()) || file.id == text);
+}
+
+const filterByShared = (shared) => {
+    return (data) => {
+        console.log(shared, data);
+        const data2 = data.filter(file => file.shared == shared 
+            || (shared ? file.sharedWith?.length > 0 : file.sharedWith?.length == 0));
+            console.log(data2);
+        return data2;
+    }
 }
 
 const sortByDate = (order) => {
@@ -71,6 +82,8 @@ const SearchBar = ({ createFileLoader, setFileLoader  }) => {
     const [ sortName, setSortName ] = useState('asc');
     const [ disableSort, setDisableSort ] = useState(false);
     const [ sortDate, setSortDate ] = useState('asc');
+    const [ shared, setShared ] = useState(false);
+    const searchRef = useRef();
 
     const handleSearch = handleKeyUpWithTimeout(async (e) => {
         setDisableSort(e.target.value != '');
@@ -97,9 +110,32 @@ const SearchBar = ({ createFileLoader, setFileLoader  }) => {
         loadInfo();
     }, [ sortDate, setSortDate, setFileLoader, createFileLoader ])
 
+    const handleShared = useCallback(async () => {
+        const newShared = !shared;
+        setShared(newShared);
+
+        const loadInfo = await createFileLoader(filterByShared(newShared));
+        setFileLoader(loadInfo);
+        loadInfo();
+    }, [ shared, setFileLoader, createFileLoader ]);
+
+    const resetSortingAndFiltering = useCallback(async () => {
+        setSortName('asc');
+        setSortDate('asc');
+        setShared(false);
+        setDisableSort(false);
+        searchRef.current.value = '';
+        handleSearch({ target: searchRef.current });
+
+        const loadInfo = await createFileLoader();
+        setFileLoader(loadInfo);
+        loadInfo();
+    }, [ setSortName, setSortDate, setDisableSort, setFileLoader, createFileLoader, searchRef, handleSearch ]);
+
+    useWhatChanged([ sortName, sortDate, shared ]);
     return (
         <div className="d-flex justify-content-center align-items-center mb-4 gap-1">
-            <input type="text" placeholder="Enter Search" className='search-input' onKeyUp={handleSearch}/>
+            <input type="text" placeholder="Enter Search" className='search-input' ref={searchRef} onKeyUp={handleSearch}/>
             <div className={`sort-icons ${disableSort && 'sort-icons-disable'}`}>
                 { sortName == 'asc' && 
                 <ToolTip toolTipMessage='Sort by name A to Z' placement='top'>
@@ -119,6 +155,23 @@ const SearchBar = ({ createFileLoader, setFileLoader  }) => {
 
                 { sortDate == 'desc' && <ToolTip toolTipMessage='Sort by date newest to oldest' placement='top'>
                     <i className='bi bi-sort-numeric-up' onClick={handleSortByDate}/>
+                </ToolTip> }
+            </div>
+
+            <div className={`sort-icons ${disableSort && 'sort-icons-disable'}`}>
+                { !shared && <ToolTip toolTipMessage='Show all files' placement='top'>
+                    <i className='bi bi-people' onClick={handleShared}/>
+                </ToolTip> }
+
+                { shared && <ToolTip toolTipMessage='Show shared files' placement='top'>
+                    <i className='bi bi-people-fill' onClick={handleShared}/>
+                </ToolTip> }
+            </div>
+
+            <div>
+                { (searchRef?.current?.value || sortDate != 'asc' || sortName != 'asc' || shared) && 
+                <ToolTip toolTipMessage='Reset sorting and filtering' placement='top'> 
+                    <i className='bi bi-arrow-repeat sort-icons' onClick={resetSortingAndFiltering}/>
                 </ToolTip> }
             </div>
         </div>
